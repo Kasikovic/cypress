7a7ba8dd087b10588b0edb921f1aaec64c5dd714
StojankaKaurin <kaurinstojanka@gmail.com>
2023-03-07 21:18:48 +0100

    login tests

---
+++ cypress/e2e/Login.cy.js
@@ -0,0 +1,39 @@
+/// <reference types="Cypress" />
+
+describe("login tests", () => {
+    it("login with unregistered user", () => {
+        cy.visit("https://gallery-app.vivifyideas.com/")
+        cy.get("a[href='/login']").click();
+        cy.get("#email").type("random@mail.com");
+        cy.get("#password").type("test123");
+        cy.get("button").click();
+        cy.url().should("contain", "/login");
+    })
+
+    it("register without email address provided", () => {
+        cy.visit("https://gallery-app.vivifyideas.com/");
+        cy.get("a[href='/login']").click();
+        cy.get("#password").type("test123");
+        cy.get("button").click();
+        cy.url().should("contain", "/login");
+    })
+
+    it("register without password", () => {
+        cy.visit("https://gallery-app.vivifyideas.com/");
+        cy.get("a[href='/login']").click();
+        cy.get('#email').type("random@mail.com");
+        cy.get("button").click();
+        cy.url().should("contain", "/login");
+    })
+
+
+    it("login with valid credentials", () => {
+        cy.visit("https://gallery-app.vivifyideas.com/");
+       // cy.get("a[href='/login']").click();
+       cy.get(".nav-link").eq(1).click();
+       cy.get("#email").type("test123456@gmail.com");
+       cy.get("#password").type("test123");
+       cy.get("button").click();
+    })
+});
+
